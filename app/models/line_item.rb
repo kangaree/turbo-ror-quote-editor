437fd5b530e7f535442a91c0be3c08383624594e
kangaree <garyzkang@gmail.com>
2024-05-15 11:37:36 -0400

    Broadcast update and delete for line items

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -7,6 +7,13 @@ class LineItem < ApplicationRecord
 
     delegate :quote, to: :line_item_date
 
+    # everything but create
+    broadcasts_to ->(line_item) { [ line_item.line_item_date, "line_items" ] }, inserts_by: :prepend
+    # TODO: fix "undefined local variable or method 'quote'"
+    # create doesn't work yet
+    after_create_commit ->(line_item) { broadcast_prepend_later_to [ line_item.line_item_date, "line_items" ], locals: { line_item: line_item, quote: line_item.quote, line_item_date: line_item.line_item_date } }
+    after_update_commit ->(line_item) { broadcast_replace_later_to [ line_item.line_item_date, "line_items" ], locals: { line_item: line_item, quote: line_item.quote, line_item_date: line_item.line_item_date } }
+    
     def total_price
       quantity * unit_price
     end
